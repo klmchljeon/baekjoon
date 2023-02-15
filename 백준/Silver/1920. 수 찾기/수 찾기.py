d58ca44f680c3c3ac35ef806223070a3c658c9c0
klmchljeon <102343371+klmchljeon@users.noreply.github.com>
2023-02-16 02:10:56 +0900

    [Silver IV] Title: 수 찾기, Time: 232 ms, Memory: 149284 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,20 +1,9 @@
-#수 찾기
 n = int(input())
-d = list(map(int,input().split()))
-d.sort()
-
-m = int(input())
-lst = list(map(int,input().split()))
-
-for i in range(m):
-    lo,hi = 0,n
-    while lo+1 < hi:
-        mid = (lo+hi)//2
-
-        if d[mid] <= lst[i]:
-            lo = mid
-
-        else:
-            hi = mid
-
-    print(1 if d[lo] == lst[i] else 0)
+d = set(map(int,input().split()))
+x = int(input())
+m = list(map(int,input().split()))
+for i in m:
+	if i in d:
+		print(1)
+	else:
+		print(0)
