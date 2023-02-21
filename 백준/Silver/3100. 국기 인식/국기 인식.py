190e80807a8fd70f919d62b97e2938c9a3761ab7
klmchljeon <102343371+klmchljeon@users.noreply.github.com>
2023-02-21 14:55:22 +0900

    [Silver IV] Title: 국기 인식, Time: 64 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/3100. 국기 인식/국기 인식.py
+++ 백준/Silver/3100. 국기 인식/국기 인식.py
@@ -1,41 +1,44 @@
-alpha = [chr(i+ord('A')) for i in range(26)]
+#국기 인식
+def hor(s,e):
+    dic = dict(zip(al,[0]*26))
+    for i in range(s,e):
+        for j in range(9):
+            st = d[i][j]
+            dic[st] += 1
+
+    res = sorted(dic.items(),key=f)[:3]
+    return res
+
+def ver(s,e):
+    dic = dict(zip(al,[0]*26))
+    for i in range(6):
+        for j in range(s,e):
+            st = d[i][j]
+            dic[st] += 1
+
+    res = sorted(dic.items(),key=f)[:3]
+    return res
+
+def cal(lst):
+    res = 0
+    for i in range(3):
+        for j in range(3):
+            for k in range(3):
+                fir = lst[0][i][0] == lst[1][j][0]
+                sec = lst[1][j][0] == lst[2][k][0]
+                if fir or sec: continue
+
+                cnt = lst[0][i][1] + lst[1][j][1] + lst[2][k][1]
+                res = max(res,cnt)
+
+    return 54 - res
+
+al = [chr(i+ord('A')) for i in range(26)]
+f = lambda x:-x[1]
 
 d = [input() for _ in range(6)]
 
-res = 0
-for f in alpha:
-    for s in alpha:
-        for t in alpha:
-            if f==s or s==t: continue
+h = [hor(i,i+2) for i in range(0,6,2)]
+v = [ver(i,i+3) for i in range(0,9,3)]
 
-            cnt = 0
-            for i in range(0,2):
-                for j in range(9):
-                    cnt += d[i][j] == f
-
-            for i in range(2,4):
-                for j in range(9):
-                    cnt += d[i][j] == s
-
-            for i in range(4,6):
-                for j in range(9):
-                    cnt += d[i][j] == t
-
-            res = max(res,cnt)
-
-            cnt = 0
-            for i in range(6):
-                for j in range(0,3):
-                    cnt += d[i][j] == f
-
-            for i in range(6):
-                for j in range(3,6):
-                    cnt += d[i][j] == s
-
-            for i in range(6):
-                for j in range(6,9):
-                    cnt += d[i][j] == t
-
-            res = max(res,cnt)
-
-print(54-res)
+print(min(cal(h),cal(v)))
