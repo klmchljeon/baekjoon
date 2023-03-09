9a092997e445168492ee7c7b9216896b3d11cca3
klmchljeon <102343371+klmchljeon@users.noreply.github.com>
2023-03-09 12:10:23 +0900

    [Platinum V] Title: 버블 소트, Time: 3600 ms, Memory: 221052 KB -BaekjoonHub

---
+++ 백준/Platinum/1517. 버블 소트/버블 소트.py
@@ -0,0 +1,46 @@
+#inversion counting
+def update(idx,val,s,e,node=1):
+    if s==e:
+        tree[node] += val
+        return 
+
+    m = (s+e)//2
+    if idx <= m:
+        update(idx,val,s,m,node*2)
+    else:
+        update(idx,val,m+1,e,node*2+1)
+
+    tree[node] += val
+    return 
+
+def cal(l,r,s,e,node=1):
+    if e<l or r<s:
+        return 0
+
+    if l<=s and e<=r:
+        return tree[node]
+
+    m = (s+e)//2
+    left = cal(l,r,s,m,node*2)
+    right = cal(l,r,m+1,e,node*2+1)
+    return left + right
+
+n = int(input())
+lst = [0] + list(map(int,input().split()))
+
+#좌표 압축
+d = []
+for i in range(1,n+1):
+    d.append((i,lst[i]))
+
+t = lambda x:x[1]
+d.sort(key = t)
+
+tree = [0]*(n*4)
+
+res = 0
+for i in range(n-1,-1,-1):
+    res += cal(1,d[i][0],1,n)
+    update(d[i][0],1,1,n)
+
+print(res)
