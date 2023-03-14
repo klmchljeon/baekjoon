0fa14f3ae313c2a1bced3b925f0eae6e64a3a725
klmchljeon <102343371+klmchljeon@users.noreply.github.com>
2023-03-14 19:33:54 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 338.83 ms, Memory: 43.7 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,11 +1,11 @@
 def solution(elements):
-    lst = [False]*(1000001)
+    st = set()
     n = len(elements)
     for i in range(n):
         tmp = 0
         for j in range(i,i+n):
             tmp += elements[j%n]
-            lst[tmp] = True
+            st.add(tmp)
     
-    answer = lst.count(True)
+    answer = len(st)
     return answer
