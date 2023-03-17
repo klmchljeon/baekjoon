617a34d698ee59b72e952ae4935867d96fe78b9c
klmchljeon <102343371+klmchljeon@users.noreply.github.com>
2023-03-17 21:38:25 +0900

    [unrated] Title: 카드 뭉치, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/159994. 카드 뭉치/README.md
+++ 프로그래머스/unrated/159994. 카드 뭉치/README.md
@@ -1,10 +1,10 @@
 # [unrated] 카드 뭉치 - 159994 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159994#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159994) 
 
 ### 성능 요약
 
-메모리: 9.93 MB, 시간: 0.00 ms
+메모리: 10.4 MB, 시간: 0.01 ms
 
 ### 구분
 
--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
@@ -4,11 +4,14 @@ def solution(cards1, cards2, goal):
     a = deque(cards1)
     b = deque(cards2)
     
-    for i in goal:
-        if a and a[0] == i:
+    n = len(goal)
+    for i in range(n):
+        if a and a[0] == goal[0]:
             a.popleft()
+            goal.pop(0)
             
-        elif b and b[0] == i:
+        elif b and b[0] == goal[0]:
+            goal.pop(0)
             b.popleft()
             
         else:
