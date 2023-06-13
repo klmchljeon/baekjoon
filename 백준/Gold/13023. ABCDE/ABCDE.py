e3a60667683a1c95b424d41d1a85b27796c1c48d
klmchljeon <102343371+klmchljeon@users.noreply.github.com>
2023-06-13 12:02:25 +0900

    [Gold V] Title: ABCDE, Time: 840 ms, Memory: 119140 KB -BaekjoonHub

---
+++ 백준/Gold/13023. ABCDE/ABCDE.py
@@ -0,0 +1,32 @@
+#ABCDE 16:13
+import sys
+sys.setrecursionlimit(3000)
+input = sys.stdin.readline
+
+def dfs(x,st:set):
+    global res
+    for nx in d[x]:
+        if not nx in st:
+            st.add(nx)
+            if len(st) == 5:
+                res = 1
+                return 
+            
+            dfs(nx,st)
+            st.remove(nx)
+
+    return 
+
+n,m = map(int,input().split())
+d = [[] for _ in range(n)]
+
+for _ in range(m):
+    a,b = map(int,input().split())
+    d[a].append(b)
+    d[b].append(a)
+
+res = 0
+for i in range(n):
+    dfs(i,set([i]))
+
+print(res)
