3a51690f6c6e060d8a1801e76e0acae4fc197faa
klmchljeon <102343371+klmchljeon@users.noreply.github.com>
2023-07-11 15:33:48 +0900

    [Platinum III] Title: Cow Lineup, Time: 76 ms, Memory: 4132 KB -BaekjoonHub

--- 백준/Platinum/5851. Cow Lineup/Cow Lineup.cc
+++ 백준/Platinum/5851. Cow Lineup/Cow Lineup.cc
@@ -3,7 +3,6 @@
 #define ll long long
 using namespace std;
 
-map<pair<int,int>,int> mp;
 map<int,int> cnt;
 
 int d[100000];
@@ -19,37 +18,29 @@ int main() {
     cin >> n >> k;
     for (i=0; i<n; i++) {
         cin >> d[i];
-        if (cnt.find(d[i]) == cnt.end()) {
-            cnt.insert({d[i],0});
-        }
     }
 
     e = 0;
     for (s=0; s<n; s++) {
         while (e<n) {
-            if (cnt[d[e]] != 0) {
-                mp.erase({-cnt[d[e]],d[e]});
-            } 
-            
-            if (mp.size() == k+1) {
-                break;
+            if (cnt.find(d[e])==cnt.end()) {
+                if (cnt.size() == k+1) {
+                    break;
+                }
+                else {
+                    cnt.insert({d[e],0});
+                }
             }
-            
+
             cnt[d[e]]++;
 
-            mp.insert({{-cnt[d[e]],d[e]},0});
+            res = max(res,cnt[d[e]]);
             e++;
         }
 
-        for (auto i:mp) {
-            res = max(res,-i.first.first);
-            break;
-        }
-
-        mp.erase({-cnt[d[s]],d[s]});
         cnt[d[s]]--;
-        if (cnt[d[s]] != 0) {
-            mp.insert({{-cnt[d[s]],d[s]},0});
+        if (cnt[d[s]] == 0) {
+            cnt.erase(d[s]);
         }
     }
 
