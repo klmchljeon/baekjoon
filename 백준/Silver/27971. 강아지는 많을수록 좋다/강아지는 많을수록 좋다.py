a498ca4539c59adf3e7e2defc77c367820ac92d2
klmchljeon <102343371+klmchljeon@users.noreply.github.com>
2023-07-11 18:02:30 +0900

    [Silver I] Title: 강아지는 많을수록 좋다, Time: 140 ms, Memory: 116056 KB -BaekjoonHub

--- 백준/Silver/27971. 강아지는 많을수록 좋다/강아지는 많을수록 좋다.py
+++ 백준/Silver/27971. 강아지는 많을수록 좋다/강아지는 많을수록 좋다.py
@@ -1,42 +1,22 @@
 max_ = int(1e9)
-N, M, A, B = map(int, (input().split()))
+n,m,a,b = map(int,input().split())
 
-if A > B:
-    temp = A
-    A = B
-    B = temp
+danger = [0]*(n+1)
+for _ in range(m):
+    l,r = map(int,input().split())
+    for i in range(l,r+1):
+        danger[i] = 1
 
-dp = [max_]*(N+1)
-danger = [0]*(N+1)
+dp = [max_]*(n+1)
+dp[0] = 0
 
-for _ in range(M):
-    a, b = map(int, input().split())
-    for i in range(b-a+1):
-        danger[i+a] = 1
-if danger[B] != 1:
-    dp[B] = 1
-if danger[A] != 1:
-    dp[A] = 1
+for i in range(1,n+1):
+    if danger[i]: continue
 
-for i in range(N+1):
-    if danger[i] == 1:
-        continue
+    if i >= a:
+        dp[i] = min(dp[i], dp[i-a]+1)
 
-    if i >= B:
-        if dp[i-A] != 0 and dp[i-B] != 0:
-            dp[i] = min(dp[i], dp[i-A] + 1, dp[i-B] + 1)
-        elif dp[i-A] != 0:
-            dp[i] = min(dp[i], dp[i-A]+1)
-        elif dp[i-B] != 0:
-            dp[i] = min(dp[i], dp[i-B]+1)
-        else:
-            continue
+    if i >= b:
+        dp[i] = min(dp[i], dp[i-b]+1)
 
-    elif i >= A and dp[i-A] != 0:
-        dp[i] = min(dp[i], dp[i-A]+1)
-
-
-if dp[N] == max_:
-    print(-1)
-else:
-    print(dp[N])
+print(dp[n] if dp[n]!=max_ else -1)
