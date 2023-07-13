04e781096f53f9753f3fa68374284df5e54b7692
klmchljeon <102343371+klmchljeon@users.noreply.github.com>
2023-07-13 14:06:04 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 276 ms, Memory: 122804 KB -BaekjoonHub

---
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
@@ -0,0 +1,40 @@
+#쉬운 최단거리
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+dx = (-1,1,0,0)
+dy = (0,0,-1,1)
+
+n,m = map(int,input().split())
+d = [list(map(int,input().split())) for _ in range(n)]
+
+visit = [[-1]*m for _ in range(n)]
+loc = None
+for i in range(n):
+    for j in range(m):
+        if d[i][j] == 2:
+            loc = (i,j)
+
+        elif d[i][j] == 0:
+            visit[i][j] = 0
+
+x,y = loc
+visit[x][y] = 0
+
+queue = deque([loc])
+while queue:
+    x,y = queue.popleft()
+
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        if not (0<=nx<n and 0<=ny<m): continue
+
+        if visit[nx][ny]==-1 and d[nx][ny]:
+            visit[nx][ny] = visit[x][y] + 1
+            queue.append((nx,ny))
+
+for i in visit:
+    print(*i)
