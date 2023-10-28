1c1d248114639c28c5d96531613f0514eb72b82a
klmchljeon <102343371+klmchljeon@users.noreply.github.com>
2023-10-28 15:50:13 +0900

    [Bronze II] Title: 분산처리, Time: 1564 ms, Memory: 111372 KB -BaekjoonHub

---
+++ 백준/Bronze/1009. 분산처리/분산처리.py
@@ -0,0 +1,13 @@
+t = int(input())
+for case in range(t):
+    a,b = map(int,input().split())
+
+    tmp = a%10
+    for i in range(b-1):
+        tmp *= a
+        tmp %= 10
+
+    if tmp == 0:
+        print(10)
+    else:
+        print(tmp)
