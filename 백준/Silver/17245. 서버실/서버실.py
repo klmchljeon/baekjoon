b6fc5ec81de7eaf4d42b130820a79651cfb88abd
klmchljeon <102343371+klmchljeon@users.noreply.github.com>
2024-01-19 17:56:33 +0900

    [Silver II] Title: 서버실, Time: 4888 ms, Memory: 70956 KB -BaekjoonHub

---
+++ 백준/Silver/17245. 서버실/서버실.py
@@ -0,0 +1,27 @@
+def check(num):
+    c = 0
+    for i in d:
+        c += min(i,num)
+
+    return half <= c
+
+n = int(input())
+d = []
+for i in range(n):
+    tmp = list(map(int,input().split()))
+    d.extend(tmp)
+
+sum_ = sum(d)
+half = sum_//2 + sum_%2
+
+s,e = -1,int(1e8)
+while s+1<e:
+    mid = (s+e)//2
+
+    if check(mid):
+        e = mid
+
+    else:
+        s = mid
+
+print(e)
