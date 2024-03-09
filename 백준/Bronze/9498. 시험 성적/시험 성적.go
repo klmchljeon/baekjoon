04eedebbb5ee645432cb968b0e5439b1ffcc4f35
klmchljeon <102343371+klmchljeon@users.noreply.github.com>
2024-03-09 10:20:34 +0900

    [Bronze V] Title: 시험 성적, Time: 8 ms, Memory: 900 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.go
@@ -0,0 +1,17 @@
+package main
+import "fmt"
+func main() {
+    var score int
+    fmt.Scan(&score)
+    if score>=90 {
+        fmt.Println("A")
+    } else if score>=80 {
+        fmt.Println("B")
+    } else if score>=70 {
+        fmt.Println("C")
+    } else if score>=60 {
+        fmt.Println("D")
+    } else {
+        fmt.Println("F")
+    }
+}
