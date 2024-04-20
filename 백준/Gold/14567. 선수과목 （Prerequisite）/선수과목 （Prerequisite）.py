b9a884bc283eb751e8f09a7adb0e578b041b61d2
klmchljeon <102343371+klmchljeon@users.noreply.github.com>
2024-04-20 22:05:02 +0900

    [Gold V] Title: 선수과목 (Prerequisite), Time: 468 ms, Memory: 142836 KB -BaekjoonHub

---
+++ 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+n,m = map(int,input().split())
+graph = [[] for _ in range(n+1)]
+degree = [0]*(n+1)
+for _ in range(m):
+    a,b = map(int,input().split())
+    graph[a].append(b)
+    degree[b] += 1
+
+queue = deque([])
+for i in range(1,n+1):
+    if degree[i] == 0:
+        queue.append(i)
+
+res = [1]*(n+1)
+while queue:
+    x = queue.popleft()
+    for nx in graph[x]:
+        degree[nx] -= 1
+        if degree[nx] == 0:
+            queue.append(nx)
+            res[nx] = res[x] + 1
+
+print(*res[1:])
