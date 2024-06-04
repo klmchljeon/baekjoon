3ab8bd5e9b3794af6092420ddd62fbb6e23ed4c7
klmchljeon <102343371+klmchljeon@users.noreply.github.com>
2024-06-05 07:18:16 +0900

    [Gold IV] Title: Autostrady, Time: 484 ms, Memory: 127572 KB -BaekjoonHub

---
+++ 백준/Gold/8549. Autostrady/Autostrady.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+
+def find(n):
+    if parent[n] == n: return n
+    return find(parent[n])
+
+v,e = map(int,input().split())
+parent = [i for i in range(v+1)]
+d = []
+for i in range(e):
+    d.append(list(map(int,input().split())))
+
+d.sort(key = lambda x:x[2])
+
+result = 0
+for a,b,c in d:
+    pa = find(a)
+    pb = find(b)
+
+    if pa!=pb:
+        if pa>pb:
+            parent[pa] = pb
+        else:
+            parent[pb] = pa
+            
+        result = max(result,c)
+
+print(result)
