a02d0907e2d7df8b69d2e803692fc63a0173d59f
klmchljeon <102343371+klmchljeon@users.noreply.github.com>
2024-06-15 07:51:27 +0900

    [Gold III] Title: 선분 교차 1, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/17386. 선분 교차 1/선분 교차 1.py
@@ -0,0 +1,22 @@
+def conv(lst):
+    return lst[:2],lst[2:]
+
+def ccw(p1,p2,p3):
+    x1,y1 = p1
+    x2,y2 = p2
+    x3,y3 = p3
+    return (x1*y2 + x2*y3 + x3*y1) - (x2*y1 + x3*y2 + x1*y3)
+
+def intersect(l1,l2):
+    p1,p2 = conv(l1)
+    p3,p4 = conv(l2)
+
+    ccw1 = ccw(p1,p2,p3)*ccw(p1,p2,p4)
+    ccw2 = ccw(p3,p4,p1)*ccw(p3,p4,p2)
+
+    return ccw1<=0 and ccw2<=0
+
+l1 = list(map(int,input().split()))
+l2 = list(map(int,input().split()))
+
+print(int(intersect(l1,l2)))
