d4977d621bfe8dd30a155bfbc4d624cdb932a5bc
klmchljeon <102343371+klmchljeon@users.noreply.github.com>
2024-08-03 15:40:47 +0900

    [Silver III] Title: N과 M (1), Time: 1772 ms, Memory: 140936 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,18 +1,19 @@
-def bt(leng):
-    #leng == 목표 수열의 길이
-    if len(s) == leng:
-        print(*s)
+def check(lst):
+    return len(set(lst)) == m
+
+def bf():
+    if len(s) == m:
+        if check(s):
+            print(*s)
+        
         return 
-    
-    #트리의 선택지를 살펴봄
+
     for i in range(1,n+1):
-        #선택할 수 있는 선택지임?
-        if not i in s:
-            s.append(i) #선택
-            bt(leng)
-            s.pop() #돌아오기
+        s.append(i)
+        bf()
+        s.pop()
 
 n,m = map(int,input().split())
 
-s = [] #현재 내가 선택한 노드들(==지금까지 만든 수열)
-bt(m)
+s = []
+bf()
