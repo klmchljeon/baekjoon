7af8d3e4caefead5f91c91a0ff78300af049f849
klmchljeon <102343371+klmchljeon@users.noreply.github.com>
2024-10-04 15:30:44 +0900

    [Gold V] Title: DKSH를 찾아라, Time: 140 ms, Memory: 45996 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/25427. DKSH를 찾아라/DKSH를 찾아라.py
+++ 백준/Gold/25427. DKSH를 찾아라/DKSH를 찾아라.py
@@ -1,14 +1,18 @@
-dic = dict(zip('DKSH',range(1,5)))
+N = int(input())
+S = list(input())
+dp = [[0]*4 for _ in range(N)]
+if S[0] == 'D' :
+    dp[0] = [1, 0, 0, 0]
+for i in range(1, N) :
+    dp[i] = dp[i-1][::]
+    if S[i] == 'D' :
+        dp[i][0] += 1
+    elif S[i] == 'K' :
+        dp[i][1] += dp[i-1][0] 
+    elif S[i] == 'S' :
+        dp[i][2] += dp[i-1][1] 
+    elif S[i] == 'H' :
+        dp[i][3] += dp[i-1][2]
 
-n = int(input())
-st = [''] + list(input())
-
-dp = [[1] + [0]*4 for _ in range(n+1)]
-for i in range(1,n+1):
-    for j in range(1,5):
-        dp[i][j] = dp[i-1][j]
-
-    if st[i] in dic:
-        dp[i][dic[st[i]]] += dp[i][dic[st[i]]-1]
-
-print(dp[n][4])
+ans = dp[N-1][3]
+print(ans)
