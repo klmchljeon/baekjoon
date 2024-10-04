57d326198563439fd00d5739622bcf309051053b
klmchljeon <102343371+klmchljeon@users.noreply.github.com>
2024-10-04 14:40:35 +0900

    [Gold II] Title: 더워!, Time: 296 ms, Memory: 147624 KB -BaekjoonHub

--- 백준/Gold/32360. 더워！/더워！.py
+++ 백준/Gold/32360. 더워！/더워！.py
@@ -1,65 +1,41 @@
+import sys
+input = lambda : sys.stdin.readline()
 from collections import deque
 
-dx = (-1,1,0,0,0)
-dy = (0,0,-1,1,0)
+dr = (-1, 1, 0, 0)
+dc = (0, 0, -1, 1)
+def bfs(home) :
+    Q = deque([home])
+    B = [[100]*M for _ in range(N)]
+    while Q :
+        r, c, d, b = Q.popleft()
+        if lst[r][c] == 'H':
+            nb = max(0, b-K)
+            if B[r][c] > nb:
+                B[r][c] = nb
+                Q.append((r, c, d+1, nb))
+        for i in range(4):
+            nr = r + dr[i]
+            nc = c + dc[i]
+            if 0 <= nr < N and 0 <= nc < M and lst[nr][nc] != '#':
+                if lst[nr][nc] == 'H' :
+                    nb = max(0, b-K)
+                else :
+                    nb = min(100, b+C)
+                if B[nr][nc] > nb:
+                    B[nr][nc] = nb
+                    Q.append((nr, nc, d+1, nb))
+                    if lst[nr][nc] == 'E' :
+                        return d+1
+    return -1
 
-def check(loc):
-    x,y = loc
-    return 0<=x<n and 0<=y<m
-
-def find(loc):
-    res = int(1e9)
-    x,y = loc
-    for k in visited[x][y]:
-        if k != -1:
-            res = min(res,k)
-
-    return res
-
-n,m = map(int,input().split())
-k,c = map(int,input().split())
-lst = [list(input()) for _ in range(n)]
-
-s,e = None,None
-for i in range(n):
-    for j in range(m):
-        if lst[i][j] == 'S':
-            s = (i,j)
-        
-        elif lst[i][j] == 'E':
-            e = (i,j)
-
-x,y = s
-visited = [[[-1]*100 for _ in range(m)] for _ in range(n)]
-visited[x][y][0] = 0
-
-queue = deque([(x,y,0)])
-while queue:
-    x,y,u = queue.popleft()
-
-    if (x,y) == e:
-        print(find(e))
-        break
-
-    for i in range(5):
-        nx = x + dx[i]
-        ny = y + dy[i]
-
-        if not check((nx,ny)): continue
-        if lst[nx][ny] == '#': continue
-
-        nu = None
-        if lst[nx][ny] == 'H':
-            nu = max(0,u-k)
-        else:
-            if u+c >= 100:
-                continue
-
-            nu = u+c
-
-        if visited[nx][ny][nu] == -1:
-            visited[nx][ny][nu] = visited[x][y][u] + 1
-            queue.append((nx,ny,nu))
-
-else:
-    print(-1)
+N, M = map(int, input().split())
+K, C = map(int, input().split())
+lst = []
+for i in range(N):
+    a = list(input())
+    lst.append(a)
+    for j in range(M) :
+        if a[j] == 'S' :
+            home = (i, j, 0, 0)
+print(bfs(home))
