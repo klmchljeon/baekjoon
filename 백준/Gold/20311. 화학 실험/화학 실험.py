3bbed1e9f2d52c522c906dd273f38519bf1e2cf8
klmchljeon <102343371+klmchljeon@users.noreply.github.com>
2024-11-10 08:53:45 +0900

    [Gold V] Title: 화학 실험, Time: 272 ms, Memory: 202632 KB, Score: 40 point -BaekjoonHub

---
+++ 백준/Gold/20311. 화학 실험/화학 실험.py
@@ -0,0 +1,32 @@
+n,k = map(int,input().split())
+a = list(map(int,input().split()))
+lst = [[a[i],i+1] for i in range(k)]
+
+lst.sort()
+max_ = lst[-1][0]
+m = n//2 + n%2
+if max_ > m:
+    print(-1)
+    exit()
+
+tmp = []
+for i in range(m):
+    tmp.append(lst[-1][1])
+    lst[-1][0] -= 1
+    if lst[-1][0] == 0:
+        lst.pop()
+
+res = []
+odd = 1
+for i in range(n):
+    if odd:
+        res.append(tmp.pop())
+    else:
+        res.append(lst[-1][1])
+        lst[-1][0] -= 1
+        if lst[-1][0] == 0:
+            lst.pop()
+
+    odd ^= 1
+
+print(*res)
