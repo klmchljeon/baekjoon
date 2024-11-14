5d49cf3258a2021c7e07c623107414e10f42cf16
klmchljeon <102343371+klmchljeon@users.noreply.github.com>
2024-11-14 09:39:27 +0900

    [Silver I] Title: 꿈틀꿈틀 호석 애벌레 - 기능성, Time: 36 ms, Memory: 31120 KB, Score: 39 point -BaekjoonHub

--- 백준/Silver/20167. 꿈틀꿈틀 호석 애벌레 － 기능성/꿈틀꿈틀 호석 애벌레 － 기능성.py
+++ 백준/Silver/20167. 꿈틀꿈틀 호석 애벌레 － 기능성/꿈틀꿈틀 호석 애벌레 － 기능성.py
@@ -5,12 +5,19 @@ res = 0
 for i in range(1<<n):
     tmp = 0
     p = 0
+    idx = 0
     for j in range(n):
-        if i&(1<<j):
-            tmp += lst[j]
+        if not i&(1<<j): continue
+        if idx > j: continue
+
+        idx = j
+        while idx < n:
+            tmp += lst[idx]
+            idx += 1
             if tmp >= k:
                 p += tmp-k
                 tmp = 0
+                break
 
     res = max(res,p)
 
