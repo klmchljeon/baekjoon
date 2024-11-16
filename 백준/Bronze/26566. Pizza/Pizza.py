765804b3c10addc8eb9ad2080a40ddf1000270c3
klmchljeon <102343371+klmchljeon@users.noreply.github.com>
2024-11-17 00:12:38 +0900

    [Bronze IV] Title: Pizza, Time: 32 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Bronze/26566. Pizza/Pizza.py
@@ -0,0 +1,13 @@
+import math
+
+t = int(input())
+for case in range(t):
+    a,p1 = map(int,input().split())
+    r,p2 = map(int,input().split())
+
+    b = r**2 * math.pi
+
+    if a*p2 > b*p1:
+        print('Slice of pizza')
+    else:
+        print('Whole pizza')
