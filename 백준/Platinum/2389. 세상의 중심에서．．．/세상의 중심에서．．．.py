16c3f5e98e3cfb73307a25af0a51f82c517b49fc
klmchljeon <102343371+klmchljeon@users.noreply.github.com>
2024-12-01 13:52:06 +0900

    [Platinum I] Title: 세상의 중심에서..., Time: 184 ms, Memory: 110608 KB -BaekjoonHub

--- 백준/Platinum/2389. 세상의 중심에서．．．/세상의 중심에서．．．.py
+++ 백준/Platinum/2389. 세상의 중심에서．．．/세상의 중심에서．．．.py
@@ -1,52 +1,37 @@
-def enclosing_circle(a,b,c):
-    ax,ay = a
-    bx,by = b
-    cx,cy = c
-
-    d = 2*(ax*(by-cy) + bx*(cy-ay) + cx*(ay-by))
-    if d == 0: return None
-
-    x = 1/d * ((ax**2 + ay**2)*(by-cy) + (bx**2 + by**2)*(cy-ay) + (cx**2 + cy**2)*(ay-by))
-    y = 1/d * ((ax**2 + ay**2)*(cx-bx) + (bx**2 + by**2)*(ax-cx) + (cx**2 + cy**2)*(bx-ax))
-
-    return (x,y)
-
-def mid(a,b):
-    return (a[0]+b[0])/2,(a[1]+b[1])/2
-
-def inside(c,a,p):
-    return dist(c,a) >= dist(c,p)
+import sys
+input = sys.stdin.readline
 
 def dist(a,b):
-    return (a[0]-b[0])**2 + (a[1]-b[1])**2
+    return ((a[0]-b[0])**2 + (a[1]-b[1])**2)**0.5
 
+w = 0.1
 n = int(input())
 lst = []
 for _ in range(n):
     x,y = map(float,input().split())
     lst.append((x,y))
 
-res = int(1e9)
-loc = None
-for i in range(n-1):
-    for j in range(i+1,n):
-        circle = mid(lst[i],lst[j])
-        for k in range(n):
-            if k in (i,j): continue
+cx = 0
+cy = 0
+for x,y in lst:
+    cx += x
+    cy += y
 
-            if not inside(circle,lst[i],lst[k]):
-                tmp = enclosing_circle(lst[i],lst[j],lst[k])
-                if tmp != None:
-                    circle = tmp
+cx /= n
+cy /= n
 
-        for k in range(n):
-            if not inside(circle,lst[i],lst[k]):
-                break
+w = 0.1
+for _ in range(10000):
+    d = 0
+    m = -1
+    for i in range(n):
+        cur = dist((cx,cy),lst[i])
+        if d < cur:
+            d = cur
+            m = i
 
-        else:
-            d = dist(circle,lst[i])**0.5
-            if res > d:
-                res = d
-                loc = circle
+    cx += (lst[m][0]-cx) * w
+    cy += (lst[m][1]-cy) * w
+    w *= 0.999
 
-print(*loc,res)
+print(cx,cy,d)
