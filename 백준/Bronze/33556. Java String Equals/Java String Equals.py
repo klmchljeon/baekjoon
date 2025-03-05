382d10cb28a6b6b6e7dd3b8f473be6a26ca7b8ec
klmchljeon <102343371+klmchljeon@users.noreply.github.com>
2025-03-05 09:11:07 +0900

    [Bronze I] Title: Java String Equals, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/33556. Java String Equals/Java String Equals.py
@@ -0,0 +1,19 @@
+a = input()
+b = input()
+if a == 'null':
+    print('NullPointerException')
+    print('NullPointerException')
+
+else:
+    if b == 'null':
+        print('false')
+        print('false')
+    elif a == b:
+        print('true')
+        print('true')
+    elif a.upper() == b.upper():
+        print('false')
+        print('true')
+    else:
+        print('false')
+        print('false')
