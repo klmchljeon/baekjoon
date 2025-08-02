12911132064070d67239fdf2ab029e8587c59e34
klmchljeon <102343371+klmchljeon@users.noreply.github.com>
2025-08-02 13:50:24 +0900

    [Gold V] Title: Z, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/1074. Z/Z.py
@@ -0,0 +1,15 @@
+def s(n,r,c):
+    global result
+    if n==0:
+        return
+    else:
+        dr,mr = divmod(r,2**(n-1))
+        dc,mc = divmod(c,2**(n-1))
+        result += (2*dr + dc) * 4**(n-1)
+        s(n-1,mr,mc)
+        return
+
+n,r,c = map(int,input().split())
+result = 0
+s(n,r,c)
+print(result)
