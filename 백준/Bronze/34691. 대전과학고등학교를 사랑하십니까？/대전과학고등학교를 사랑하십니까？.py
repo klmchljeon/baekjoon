65915de9d8376da7663d46efb0dc418352f65af5
klmchljeon <102343371+klmchljeon@users.noreply.github.com>
2025-11-19 01:01:03 +0900

    [Bronze V] Title: 대전과학고등학교를 사랑하십니까?, Time: 84 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/34691. 대전과학고등학교를 사랑하십니까？/대전과학고등학교를 사랑하십니까？.py
@@ -0,0 +1,12 @@
+while True:
+    st = input()
+    if st == 'end': break
+
+    if st == 'animal':
+        print('Panthera tigris')
+
+    elif st == 'tree':
+        print('Pinus densiflora')
+
+    else:
+        print('Forsythia koreana')
