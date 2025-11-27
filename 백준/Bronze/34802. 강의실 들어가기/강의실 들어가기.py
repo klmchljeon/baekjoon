47faa44713d3e5ace601b34e0ff25512987f0875
klmchljeon <102343371+klmchljeon@users.noreply.github.com>
2025-11-27 23:55:22 +0900

    [Bronze III] Title: 강의실 들어가기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/34802. 강의실 들어가기/강의실 들어가기.py
@@ -0,0 +1,5 @@
+h1,m1,s1 = map(int,input().split(':'))
+h2,m2,s2 = map(int,input().split(':'))
+t,k = map(int,input().split())
+s = (h2-h1)*3600 + (m2-m1)*60 + (s2-s1)
+print(int(t - (t*k//100) <= s))
