242168cef1e04550b47c20b7cc3aa1aa3a3951b3
klmchljeon <102343371+klmchljeon@users.noreply.github.com>
2026-03-24 15:39:12 +0900

    [Bronze V] Title: A+B, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.py
+++ 백준/Bronze/1000. A＋B/A＋B.py
@@ -1,2 +1,244 @@
-a,b = map(int,input().split())
-print(a+b)
+st = input()
+if st == '1 1':
+    print(2)
+
+if st == '1 2':
+    print(3)
+
+if st == '1 3':
+    print(4)
+
+if st == '1 4':
+    print(5)
+
+if st == '1 5':
+    print(6)
+
+if st == '1 6':
+    print(7)
+
+if st == '1 7':
+    print(8)
+
+if st == '1 8':
+    print(9)
+
+if st == '1 9':
+    print(10)
+
+if st == '2 1':
+    print(3)
+
+if st == '2 2':
+    print(4)
+
+if st == '2 3':
+    print(5)
+
+if st == '2 4':
+    print(6)
+
+if st == '2 5':
+    print(7)
+
+if st == '2 6':
+    print(8)
+
+if st == '2 7':
+    print(9)
+
+if st == '2 8':
+    print(10)
+
+if st == '2 9':
+    print(11)
+
+if st == '3 1':
+    print(4)
+
+if st == '3 2':
+    print(5)
+
+if st == '3 3':
+    print(6)
+
+if st == '3 4':
+    print(7)
+
+if st == '3 5':
+    print(8)
+
+if st == '3 6':
+    print(9)
+
+if st == '3 7':
+    print(10)
+
+if st == '3 8':
+    print(11)
+
+if st == '3 9':
+    print(12)
+
+if st == '4 1':
+    print(5)
+
+if st == '4 2':
+    print(6)
+
+if st == '4 3':
+    print(7)
+
+if st == '4 4':
+    print(8)
+
+if st == '4 5':
+    print(9)
+
+if st == '4 6':
+    print(10)
+
+if st == '4 7':
+    print(11)
+
+if st == '4 8':
+    print(12)
+
+if st == '4 9':
+    print(13)
+
+if st == '5 1':
+    print(6)
+
+if st == '5 2':
+    print(7)
+
+if st == '5 3':
+    print(8)
+
+if st == '5 4':
+    print(9)
+
+if st == '5 5':
+    print(10)
+
+if st == '5 6':
+    print(11)
+
+if st == '5 7':
+    print(12)
+
+if st == '5 8':
+    print(13)
+
+if st == '5 9':
+    print(14)
+
+if st == '6 1':
+    print(7)
+
+if st == '6 2':
+    print(8)
+
+if st == '6 3':
+    print(9)
+
+if st == '6 4':
+    print(10)
+
+if st == '6 5':
+    print(11)
+
+if st == '6 6':
+    print(12)
+
+if st == '6 7':
+    print(13)
+
+if st == '6 8':
+    print(14)
+
+if st == '6 9':
+    print(15)
+
+if st == '7 1':
+    print(8)
+
+if st == '7 2':
+    print(9)
+
+if st == '7 3':
+    print(10)
+
+if st == '7 4':
+    print(11)
+
+if st == '7 5':
+    print(12)
+
+if st == '7 6':
+    print(13)
+
+if st == '7 7':
+    print(14)
+
+if st == '7 8':
+    print(15)
+
+if st == '7 9':
+    print(16)
+
+if st == '8 1':
+    print(9)
+
+if st == '8 2':
+    print(10)
+
+if st == '8 3':
+    print(11)
+
+if st == '8 4':
+    print(12)
+
+if st == '8 5':
+    print(13)
+
+if st == '8 6':
+    print(14)
+
+if st == '8 7':
+    print(15)
+
+if st == '8 8':
+    print(16)
+
+if st == '8 9':
+    print(17)
+
+if st == '9 1':
+    print(10)
+
+if st == '9 2':
+    print(11)
+
+if st == '9 3':
+    print(12)
+
+if st == '9 4':
+    print(13)
+
+if st == '9 5':
+    print(14)
+
+if st == '9 6':
+    print(15)
+
+if st == '9 7':
+    print(16)
+
+if st == '9 8':
+    print(17)
+
+if st == '9 9':
+    print(18)
+
